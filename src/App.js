4a576ef3a69307ecd5a81e7e7e2fc61756220014
Madi Rysbek <madirysbek@yandex.kz>
2022-07-16 20:03:03 +0600

    feat: render order list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from "react";
 import "antd/dist/antd.css";
-import { Layout } from "antd";
+import { Layout, List } from "antd";
+
+import orders from './data/NeRelog_apps.json'
+import clients from './data/NeRelog_clients.json'
 
 const { Sider, Content } = Layout
 
 const App = () => {
+  function getClient(id) {
+    return clients.find(el => el.id === id)
+  }
   return (
     <Layout>
-      <Sider width={200}>
-        <ul>
-          <li>one</li>
-          <li>two</li>
-          <li>three</li>
-        </ul>
+      <Sider width={250} theme="light">
+        <List
+          size="small"
+          dataSource={orders}
+          renderItem={item => <List.Item key={item.id}>
+            <List.Item.Meta
+              title={getClient(item.client_id).name}
+              description={item.type}
+            />
+            <div>{item.price}₸</div>
+          </List.Item>}
+        />
       </Sider>
       <Content>
         <p>Content</p>
