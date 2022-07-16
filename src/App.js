f8034a9170c4d82201eb668f02ce39a66c17f627
Madi Rysbek <madirysbek@yandex.kz>
2022-07-16 17:30:23 +0600

    antd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import "antd/dist/antd.css";
+import { Layout } from "antd";
+
+const { Sider, Content } = Layout
 
 const App = () => {
   return (
-    <div>Hello React App</div>
-  )
-}
+    <Layout>
+      <Sider width={200}>
+        <ul>
+          <li>one</li>
+          <li>two</li>
+          <li>three</li>
+        </ul>
+      </Sider>
+      <Content>
+        <p>Content</p>
+      </Content>
+    </Layout>
+  );
+};
 
-export default App
+export default App;
